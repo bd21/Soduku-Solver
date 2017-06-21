7f7eb1d778f361afdda73fe081c6049ea81f03c5
Demosthenes <bd21@uw.edu>
2017-06-21 15:30:33 -0700

    optimizer

--- main.py
+++ main.py
@@ -51,6 +51,26 @@ squared_deltas = tf.square(linear_model-y)
 loss = tf.reduce_sum(squared_deltas)
 print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]})) # this is the loss value
 
+# now the machine will optimize variables until we get to desired W and b values
+#   (these are -1 and 1 respectively
+
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+# training data
+x_train = [1,2,3,4]
+y_train = [0,-1,-2,-3]
+# training loops
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init) # reset values to wrong
+for i in range(1000):
+  sess.run(train, {x:x_train, y:y_train})
+
+# evaluate training accuracy
+curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x:x_train, y:y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+
 
 
 
